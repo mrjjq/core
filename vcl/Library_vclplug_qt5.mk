d355fabee1d34a2d7a64c051dd37443c8a7eef79
Jan-Marek Glogowski <glogow@fbihome.de>
2018-09-17 11:01:59 +0200

    Qt5 use LO provided native file pickers
    This uses the native file pickers on Windows and MacOSX.
    
    Change-Id: Ic836d3ed8de0760c6c94c68d61f6eaa96f773000

--- vcl/Library_vclplug_qt5.mk
+++ vcl/Library_vclplug_qt5.mk
@@ -85,7 +85,7 @@ endif
 $(eval $(call gb_Library_add_exception_objects,vclplug_qt5,\
     vcl/qt5/Qt5Bitmap \
     vcl/qt5/Qt5Data \
-    vcl/qt5/Qt5FilePicker \
+    $(if $(USING_X11),vcl/qt5/Qt5FilePicker) \
     vcl/qt5/Qt5Font \
     vcl/qt5/Qt5FontFace \
     vcl/qt5/Qt5Frame \
